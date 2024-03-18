54797c3e0150790d7322e931fdf23a5716fbcce1
Bendeguz Csirmaz <csirmazbendeguz@gmail.com>
2024-03-18 18:07:13 +0800

    Fix test

--- tests/test_patcher.py
+++ tests/test_patcher.py
@@ -1,9 +1,31 @@
-from importlib.metadata import Distribution
+from pathlib import Path
+
+import pytest
+from cleo.application import Application
+from cleo.io.io import IO
+from cleo.io.outputs.output import Verbosity
 
 from poetry_patches.patcher import PoetryPatcher
 
 
-def test_patcher():
-    distribution = Distribution.at("poetry_patches")
-    print("files", distribution.files)
-    PoetryPatcher.apply_patches()
+@pytest.fixture
+def io() -> IO:
+    io = Application().create_io()
+    io.set_verbosity(Verbosity.DEBUG)
+    return io
+
+
+def test_apply_patches(io: IO) -> None:
+    target_dir = Path("./patches")
+    PoetryPatcher(None, io).apply_patches(
+        target_dir,
+        [
+            "./patches/dummy_1.diff",
+            "./patches/dummy_2.diff",
+            "./patches/dummy_3.diff",
+            "./patches/dummy_4.diff",
+        ],
+    )
+
+    assert not (target_dir / "dummy.txt").exists()
+    assert not (target_dir / "dummy_2.txt").exists()
